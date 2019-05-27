c06f15e076dd0d15019ff83754affedc383deaee
Fireshadow3 <alexboss10001@gmail.com>
2019-05-27 16:09:51 +0200

    Big change

---
+++ Website/cardPage.php
@@ -0,0 +1,14 @@
+<!-- Roba delle serie tv:
+-Immagine
+-Nome
+-Anno pubblicazione
+-Descrizione
+-Genere
+-Numero di stagioni
+-Durata media degli episodi
+-Cast
+-Regista
+-->
+<html lang="en">
+    
+</html>
--- Website/tvseries.php
+++ Website/tvseries.php
@@ -1,26 +1,4 @@
 <!DOCTYPE html>
-<!--
-░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░
-░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░
-░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░
-░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░
-░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░
-░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░ 
-░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░
-░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░
-░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░
-░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░
-▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░
-▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
-▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░
-░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░
-░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░
-░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░
-░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░
-░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░
-░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░
-
--->
 <html lang="en">
     <head>
         <!-- Required meta tags -->
@@ -29,16 +7,15 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <!-- Custom CSS -->
-        <link rel="stylesheet" type="text/css" href="WebsiteThemes/CUSTOM.css">
         <link rel="stylesheet" type="text/css" href="WebsiteThemes/2_gabrib.css">
 
-        <title></title>
+        <title>Seagull review</title>
     </head>
     <body class="bg-dark">
-        <?php
-        //$db = new MysqliDb('localhost', 'alessandro', 'secret', 'dbName');
-        //echo 'ciao mamma';
-        ?>
+		<?php
+		//$db = new MysqliDb('localhost', 'alessandro', 'secret', 'dbName');
+		//echo 'ciao mamma';
+		?>
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg bg-div">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,176 +38,143 @@
             </div>
         </nav>
 
-
-        <div class="container w-50 mt-3 rounded h-50 bg-div">
-            <form class="form-inline my-2 my-lg-0 justify-content-center">
-                <input class="form-control mr-sm-2 mt-1 mb-1" type="search" placeholder="Search" aria-label="Search">
+        <!-- Container for search bar -->
+        <div class="container w-50 mt-3 rounded h-50 bg-div mx-auto">
+            <form class="form-inline my-2 my-lg-0 justify-content-center" action="" method="GET">
+                <input class="form-control mr-sm-2 mt-1 mb-1" type="search" name="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0 mt-1 mb-1 bg-btn" type="submit">Search</button>
             </form>
         </div>
 
         <!-- Container for bottom objects -->
-        <div class="container-fluid" mt-10>
+        <div class="container-fluid pt-3" mt-10>
             <div class="row">
-                <!-- Container for search options -->
-                <div class="col-2 bg-light">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                        </div>
-                        <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                        </div>
-                        <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
-                    </div>
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                        </div>
-                    </div>
-                </div>
-                <!-- Container for media objects -->
-                <div class="col-6 bg-light justify-content-md-center">
-                    <div class="row">
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="..." alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+				<?php if (isset($_GET["search"])) { ?>
+					<!-- Container for media objects -->
+					<div class="col bg-light justify-content-md-center">
+						<div class="card-deck card-deck-padding">
+							<div class="card">
+								<a href="http://www.google.it/">
+									<img class="card-img-top" src="https://is4-ssl.mzstatic.com/image/thumb/Video114/v4/e3/a0/b4/e3a0b478-bac5-e587-7e03-4d16f4d40f09/pr_source.lsr/268x0w.png" alt="Card image cap" >
+								</a>
+								<div class="card-body">
+									<h5 class="card-title">Acquaman</h5>
+									<p class="card-text">Questa è la trama di Aquaman. Lui non è un pesce ma mangia i pesci...</p>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="https://images.wallpaperscraft.com/image/city_vector_panorama_119914_3840x2160.jpg" alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Cybercity</h5>
+									<p class="card-text">Wow se questa citta è figa lorem ipsum è fantastico ma veramente...</p>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								</div>
+							</div>
+						</div>
+						<div class="card-deck card-deck-padding">
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+						</div>
+						<div class="card-deck">
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+							<div class="card">
+								<img class="card-img-top" src="..." alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+								</div>
+								<div class="card-footer">
+									<small class="text-muted">Last updated 3 mins ago</small>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php } else {
+				?>
+				<p>Ammazzati</p>
+			<?php } ?>
         </div>
 
 
